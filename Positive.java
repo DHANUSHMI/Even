435bca30bb0483b35706697de0dccdc646de1423
DHANUSHMI <dhanushmisaravanan@gmail.com>
2017-09-29 11:10:37 -0600

    Positive.java

---
+++ Positive.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class Positive
+{
+public static void main(System.in);
+{
+Scanner sc=new Scanner(System.in);
+int s=sc.nextInt();
+if(s<0)
+{
+System.out.println("Negative");
+}
+else if(s>0)
+{
+System.out.println("Positive");
+}
+else
+{
+System.out.println("Zero");
+}
+}
+}
