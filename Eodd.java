87defcae5e1b188bac118372a723a12305bf1a54
DHANUSHMI <dhanushmisaravanan@gmail.com>
2017-09-29 11:34:25 -0600

    Eodd.java

---
+++ Eodd.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class Eodd
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int s=sc.nextInt();
+if(s%2==0)
+{
+System.out.println("Even");
+}
+else
+{
+System.out.println("Odd");
+}
+}
+}
