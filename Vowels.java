1b03e9b326e063c3fd097e7bd45c5a0891575e84
DHANUSHMI <dhanushmisaravanan@gmail.com>
2017-09-29 11:42:02 -0600

    Vowels.java

---
+++ Vowels.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class Vowels
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+char ch1=s.toCharArray();
+char ch[]={'a','e','i','o','u'};
+if(ch1==ch)
+{
+System.out.println("Vowels");
+}
+else
+{
+System.out.println("Consonant");
+}
+}
+}
